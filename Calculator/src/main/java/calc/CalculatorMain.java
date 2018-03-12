5743220dcc489cc2908986603e0ccb12bd6221dc
Wen1234 <b909478174@gmail.com>
2018-03-12 01:53:14 +0100

    upload

--- Calculator/src/main/java/calc/CalculatorMain.java
+++ Calculator/src/main/java/calc/CalculatorMain.java
@@ -5,15 +5,15 @@ public class CalculatorMain {
 		
 	}
 	public int add(int a, int b){
-		int res=add(a,b);
+		int res=a+b;
 		System.out.println("Addition:"+res);
 		return a+b;
 	}
 	public int sub(int a, int b) {
-		int res=sub(a,b);
+		int res=a-b;
 		System.out.println("Subtracion::"+res);
 		return a-b;
 	}
- 
+    
 	
 }
